f37ce3a4eb60cd1ea1c037147511fd16cf0eb163
wugl <309693441@qq.com>
2025-08-16 13:48:44 +0800

    ✨ feat: 交互组件
    使用 pract，client:load

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    base: '/astro-tutorial',
-    site: 'https://supertiny99.github.io/astro-tutorial'
+  base: '/astro-tutorial',
+  site: 'https://supertiny99.github.io/astro-tutorial',
+  integrations: [preact()]
 });
