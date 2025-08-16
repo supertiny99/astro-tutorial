550f02be5f0a2086ae922ca45377bd363048bf62
wugl <309693441@qq.com>
2025-08-16 13:26:36 +0800

    ✨ feat: 添加三方依赖，增加 rss 功能

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     base: '/astro-tutorial',
+    site: 'https://supertiny99.github.io/astro-tutorial'
 });
